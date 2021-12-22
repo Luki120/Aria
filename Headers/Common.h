5deb12de3cfaba0c841b6be7e1bf908d84fad14a
luki120 <luki120@exerra.xyz>
2021-12-22 01:40:55 -0300

    common stuff file

---
+++ Headers/Common.h
@@ -0,0 +1,18 @@
+@import UIKit;
+
+
+@interface _UIBackdropView : UIView
+@property (assign, nonatomic) BOOL blurRadiusSetOnce;
+@property (copy, nonatomic) NSString *_blurQuality;
+- (id)initWithSettings:(id)arg1;
+- (id)initWithFrame:(CGRect)arg1 autosizesToFitSuperview:(BOOL)arg2 settings:(id)arg3;
+@end
+
+
+@interface _UIBackdropViewSettings : NSObject
++ (id)settingsForStyle:(long long)arg1;
+@end
+
+
+@interface NSDistributedNotificationCenter : NSNotificationCenter
+@end
